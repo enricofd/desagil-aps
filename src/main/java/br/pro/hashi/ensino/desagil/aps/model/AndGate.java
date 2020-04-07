2616da0294e824d1800ecfc18489962c33137bfd
Marcos Vinícius <marquinhosvinis@gmail.com>
2020-04-07 02:50:39 -0300

    Finalizando atividade

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
@@ -7,7 +7,7 @@ public class AndGate extends Gate {
     private final NandGate nandC;
 
     public AndGate() {
-        super("OR", 2);
+        super("AND", 2);
 
         nandA = new NandGate();
         nandB = new NandGate();
@@ -22,6 +22,20 @@ public class AndGate extends Gate {
 
     @Override
     public void connect(int inputIndex, Emitter emitter) {
+        if (inputIndex < 0 || inputIndex > 1) {
+            throw new IndexOutOfBoundsException(inputIndex);
+        }
+        if (inputIndex == 0) {
+            nandA.connect(0, emitter);
+            nandB.connect(1, emitter);
+        }
+        if (inputIndex == 1) {
+            nandB.connect(0, emitter);
+            nandA.connect(1, emitter);
+        }
+
+        nandC.connect(0, nandA);
+        nandC.connect(1, nandB);
 
 
     }
