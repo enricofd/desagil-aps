fdc200f7a4919b308f821bb76dc98287889f5cbc
marcompp <marcomoliternop@gmail.com>
2020-04-21 19:31:56 -0300

    light

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -1,6 +1,7 @@
 package br.pro.hashi.ensino.desagil.aps.view;
 
 import br.pro.hashi.ensino.desagil.aps.model.Gate;
+import br.pro.hashi.ensino.desagil.aps.model.Light;
 import br.pro.hashi.ensino.desagil.aps.model.Switch;
 
 import javax.swing.*;
@@ -14,41 +15,66 @@ import java.net.URL;
 public class GateView extends FixedPanel implements ActionListener, MouseListener {
 
     private final Gate gate;
+    private final Light light;
 
     private final JCheckBox inBox1;
     private final JCheckBox inBox2;
-    private final JCheckBox outBox;
+
+    private final Image image;
+    //private final JCheckBox outBox;
+
+
+    private int lightx;
+    private int lighty;
+    private int lightr;
+
 
     public GateView(Gate gate) {
-        super(122, 122);
+        super(180, 160);
 
         this.gate = gate;
+        this.light= new Light(255,0,0);
 
         int h = 2;
 
+        //posição da luz
+        lightx = 142;
+        lighty = 50;
+        lightr = 25;
+
         inBox1 = new JCheckBox();
         inBox2 = new JCheckBox();
-        outBox = new JCheckBox();
+        //outBox = new JCheckBox();
 
         JLabel inLabel = new JLabel("Entrada:");
         JLabel outLabel = new JLabel("Saida:");
 
-        add(inLabel, 8, h, 75, 25);
-        add(inBox1, 8, h+20, 75, 25);
-        add(outLabel, 8, h+62, 75, 25);
-        add(outBox, 8, h+82, 75, 25);
+        //add(inLabel, 8, h, 75, 25);
+        //add(inBox1, 10, h+30, 17, 20);
+        //add(outLabel, 8, h+62, 25, 23);
+        //add(outBox, 8, h+82, 75, 25);
 
 
-        outBox.setEnabled(false);
-        inBox1.addActionListener(this);
+        // Usamos esse carregamento nos Desafios, vocês lembram?
+        String name = gate.toString() + ".jpg";
+        URL url = getClass().getClassLoader().getResource(name);
+        image = getToolkit().getImage(url);
+
 
 
         if (gate.getInputSize() > 1) {
-            add(inBox2, 8, h+40, 75, 25);
+            add(inBox1, 10, h+30, 17, 20);
+            add(inBox2, 10, h+66, 17, 20);
             inBox2.addActionListener(this);
 
         }
+        else {
+            add(inBox1, 10, h+45, 17, 20);
+        }
 
+
+        //outBox.setEnabled(false);
+        inBox1.addActionListener(this);
 
 
         addMouseListener(this);
@@ -75,13 +101,9 @@ public class GateView extends FixedPanel implements ActionListener, MouseListene
         System.out.println(gate.read());
 
         //outBox.setEnabled(true);
-        if (gate.read()) {
-            outBox.setSelected(true);
-        }
-        else {
-            outBox.setSelected(false);
-        }
+        light.connect(0,gate);
         //outBox.setEnabled(false);
+        repaint();
 
     }
 
@@ -93,10 +115,21 @@ public class GateView extends FixedPanel implements ActionListener, MouseListene
 
     @Override
     public void mouseClicked(MouseEvent event) {
-        // Não precisamos de uma reação específica à ação de pressionar
-        // um botão do mouse, mas o contrato com MouseListener obriga
-        // esse método a existir, então simplesmente deixamos vazio.
 
+        // Descobre em qual posição o clique ocorreu.
+        int x = event.getX();
+        int y = event.getY();
+
+
+        // Se o clique foi dentro do quadrado colorido...
+        if (Math.abs(x-lightx) + Math.abs(y-lighty) >= lightr) {
+
+            // ...então abrimos a janela seletora de cor...
+            light.setColor(JColorChooser.showDialog(this, null, light.getColor()));
+
+            // ...e chamamos repaint para atualizar a tela.
+            repaint();
+        }
     }
 
     @Override
@@ -125,5 +158,29 @@ public class GateView extends FixedPanel implements ActionListener, MouseListene
         // Não precisamos de uma reação específica à ação do mouse
         // sair do painel, mas o contrato com MouseListener obriga
         // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        // Não podemos esquecer desta linha, pois não somos os
+        // únicos responsáveis por desenhar o painel, como era
+        // o caso nos Desafios. Agora é preciso desenhar também
+        // componentes internas, e isso é feito pela superclasse.
+        super.paintComponent(g);
+
+        // Desenha a imagem, passando sua posição e seu tamanho.
+        //g.drawImage(image, 10, 80, 221, 221, this);
+
+        // Desenha a imagem, passando sua posição e seu tamanho.
+        g.drawImage(image, 0, 0, 180, 160, this);
+
+        // Desenha um quadrado cheio.
+        g.setColor(light.getColor());
+        g.fillOval(lightx, lighty, lightr, lightr);
+
+        // Linha necessária para evitar atrasos
+        // de renderização em sistemas Linux.
+        getToolkit().sync();
     }
 }
